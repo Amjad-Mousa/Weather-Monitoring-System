5675508b88be7ef2b396e42a64b169a7310944b6
Amjad-Mousa <amjad.mousa.600@gmail.com>
2025-02-06 12:41:00 -0800

    Add JsonHelper,XmlHelper

---
+++ Weather-Monitoring-System/Helpers/JsonHelper.cs
@@ -0,0 +1,59 @@
+﻿namespace Weather_Monitoring_System
+{
+    public class JsonWeatherDataParser
+    {
+        public static WeatherData Parse(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+            {
+                throw new ArgumentException("JSON string cannot be null or empty");
+            }
+
+            try
+            {
+                var data = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(json);
+                var location = data?.Location?.ToString();
+                if (string.IsNullOrEmpty(location))
+                {
+                    throw new ArgumentException("JSON string must contain a valid Location element");
+                }
+
+                var temperature = data?.Temperature;
+                if (temperature == null)
+                {
+                    throw new ArgumentException("JSON string must contain a valid Temperature element");
+                }
+
+                var humidity = data?.Humidity;
+                if (humidity == null)
+                {
+                    throw new ArgumentException("JSON string must contain a valid Humidity element");
+                }
+
+                return new WeatherData(location, (double)temperature, (double)humidity);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Failed to parse JSON data", ex);
+            }
+        }
+
+        public static WeatherData ReadFromFile(string filePath)
+        {
+            try
+            {
+                if (!System.IO.File.Exists(filePath))
+                {
+                    throw new FileNotFoundException("File not found", filePath);
+                }
+
+                string json = System.IO.File.ReadAllText(filePath);
+                return JsonWeatherDataParser.Parse(json);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Failed to read or parse the file", ex);
+            }
+        }
+    }
+}
---
+++ Weather-Monitoring-System/Helpers/XmlHelper.cs
@@ -0,0 +1,44 @@
+﻿namespace Weather_Monitoring_System
+{
+    public class XmlWeatherDataParser
+    {
+        public static WeatherData Parse(string xml)
+        {
+            var doc = new System.Xml.XmlDocument();
+            if (string.IsNullOrEmpty(xml))
+            {
+                throw new System.ArgumentException("XML string cannot be null or empty");
+            }
+            doc.LoadXml(xml);
+
+            var locationNode = doc.GetElementsByTagName("Location").Cast<System.Xml.XmlNode>().FirstOrDefault();
+            if (locationNode == null)
+            {
+                throw new System.ArgumentException("XML string must contain a Location element");
+            }
+            var location = locationNode.InnerText;
+
+            var temperatureNode = doc.GetElementsByTagName("Temperature").Cast<System.Xml.XmlNode>().FirstOrDefault();
+            if (temperatureNode == null)
+            {
+                throw new System.ArgumentException("XML string must contain a Temperature element");
+            }
+            var temperature = double.Parse(temperatureNode.InnerText);
+
+            var humidityNode = doc.GetElementsByTagName("Humidity").Cast<System.Xml.XmlNode>().FirstOrDefault();
+            if (humidityNode == null)
+            {
+                throw new System.ArgumentException("XML string must contain a Humidity element");
+            }
+            var humidity = double.Parse(humidityNode.InnerText);
+
+            return new WeatherData(location, temperature, humidity);
+        }
+
+        public static WeatherData ReadFromFile(string filePath)
+        {
+            string xml = System.IO.File.ReadAllText(filePath);
+            return XmlWeatherDataParser.Parse(xml);
+        }
+    }
+}
