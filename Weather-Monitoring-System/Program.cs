91a31d5550e9d90b550f183e56729c258a67c210
Amjad-Mousa <amjad.mousa.600@gmail.com>
2025-02-06 11:54:05 -0800

    test

--- Weather-Monitoring-System/Program.cs
+++ Weather-Monitoring-System/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using  System;    
+public class program
+{
+    public static void Main()
+
+    {
+        var x = Console.ReadLine();
+        Console.WriteLine(x);
+        Console.WriteLine("Hello World");
+    }
+}           
